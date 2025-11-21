2e3a0212c54ab3d0301792694e8fac16f8ff7c21
Chu Sojin <chu3322@kumoh.ac.kr>
2025-11-21 11:27:46 +0900

    setting

--- cpu-scheduling-simulator/next.config.mjs
+++ cpu-scheduling-simulator/next.config.mjs
@@ -1,15 +1,17 @@
 /** @type {import('next').NextConfig} */
 
+const isProd = process.env.NODE_ENV === 'production';
 const repo = 'cpu-scheduling-simulator';
 
 const nextConfig = {
   output: 'export',
-  basePath: `/${repo}`,
-  assetPrefix: `/${repo}/`,
+  basePath: isProd ? `/${repo}` : '',
+  assetPrefix: isProd ? `/${repo}/` : '',
   images: {
     unoptimized: true,
   },
   reactCompiler: true,
+  trailingSlash: true,
 };
 
 export default nextConfig;
